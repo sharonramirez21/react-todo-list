7293720b095d9e49f0758e390b39e3c7045c83dd
sharonramirez21 <sharonramirez421@gmail.com>
2026-05-08 11:47:08 -0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-todo-list/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
